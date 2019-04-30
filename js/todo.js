75651e6f4758bb70035934a68877eb9c6f7e43a6
Rimantas <belauzas@yahoo.com>
2019-04-30 11:53:10 +0300

    Remove task and display if empty list

--- js/todo.js
+++ js/todo.js
@@ -7,10 +7,15 @@ var todo = (function(){
         $textarea = $todo.find('#new_todo_content'),
         $button = $todo.find('.btn.btn-add'),
         $list = $todo.find('#todo_list'),
-        new_task_template = $todo.find('#todo_new_task_template').html();
+        new_task_template = $todo.find('#todo_new_task_template').html(),
+        empty_task_template = $todo.find('#todo_empty_template').html();
     
     // bind events
     $button.on('click', addTask);
+    $list.on('click', 'i.remove', removeTask);
+
+    // pradinio turinio atvaizdavimas (tuscias TODO sarasas)
+    render();
 
     // sukurti (prideti)
         // textarea > 0
@@ -26,8 +31,9 @@ var todo = (function(){
     
     // pasalinti
         // remove click
-    function removeTask() {
-        console.log('removeTask - pasalina turini');
+    function removeTask( event ) {
+        var remove_index = $(event.target).parents('.todo-task').index();
+        tasks.splice(remove_index, 1);
         render();
         return;
     }
@@ -43,14 +49,18 @@ var todo = (function(){
     // atvaizduoti
         // on any update/change
     function render() {
-        var data = {
-            task_number: [],
-            task_text: tasks
-        };
-        for ( var i=0; i<tasks.length; i++ ) {
-            data.task_number.push( i+1 );
+        if ( tasks.length > 0 ) {
+            var data = {
+                task_number: [],
+                task_text: tasks
+            };
+            for ( var i=0; i<tasks.length; i++ ) {
+                data.task_number.push( i+1 );
+            }
+            return $list.html( HTML_template.render( new_task_template, data ) );
+        } else {
+            return $list.html( empty_task_template );
         }
-        return $list.html( HTML_template.render( new_task_template, data ) );
     }
 
     return {
@@ -61,5 +71,3 @@ var todo = (function(){
     }
 
 })();
-
-
