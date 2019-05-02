1e658e5ffefa6e15ebdc07ad28968e6a66756f44
Rimantas <belauzas@yahoo.com>
2019-05-02 10:31:59 +0300

    update task, cancel task editing, text validation

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,6 @@
 var todo = (function(){
 
-    var tasks = [];
+    var tasks = ['asd'];
 
     //cached DOM
     var $todo = $('#todo'),
@@ -8,14 +8,27 @@ var todo = (function(){
         $button = $todo.find('.btn.btn-add'),
         $list = $todo.find('#todo_list'),
         new_task_template = $todo.find('#todo_new_task_template').html(),
+        update_task_template = $todo.find('#todo_update_task_template').html(),
         empty_task_template = $todo.find('#todo_empty_template').html();
     
     // bind events
     $button.on('click', addTask);
     $list.on('click', 'i.remove', removeTask);
+    $list.on('click', 'i.edit', updateTask);
+    $list.on('click', 'i.cancel', cancelTask);
+    $list.on('click', 'i.save', saveTask);
 
     // pradinio turinio atvaizdavimas (tuscias TODO sarasas)
     render();
+
+    // sutvarkome teksta (saugumo prasme)
+    function secureText( text ) {
+        return text.replace(/&/g, '&amp;')
+                    .replace(/</g, '&lt;')
+                    .replace(/>/g, '&gt;')
+                    .replace(/"/g, '&quot;')
+                    .replace(/'/g, '&apos;');
+    }
 
     // sukurti (prideti)
         // textarea > 0
@@ -23,7 +36,7 @@ var todo = (function(){
     function addTask() {
         var text = $textarea.val();
         if ( text.length > 0 ) {
-            tasks.push( text );
+            tasks.push( secureText( text ) );
             $textarea.val('');
         }
         render();
@@ -40,10 +53,43 @@ var todo = (function(){
     
     // redaguoti
         // edit click
-    function updateTask() {
-        console.log('updateTask - atnaujina turini');
-        render();
-        return;
+    function updateTask( event ) {
+        var update_index = $(event.target).parents('.todo-task').index(),
+            data = {
+                task_number: [ update_index + 1 ],
+                task_text: [ tasks[update_index] ]
+            };
+        return $list.find('.todo-task').eq(update_index).replaceWith( HTML_template.render( update_task_template, data ) );
+    }
+    
+    // atsaukti redagavima
+        // cancel click
+    function cancelTask( event ) {
+        var cancel_index = $(event.target).parents('.todo-task').index(),
+            data = {
+                task_number: [ cancel_index + 1 ],
+                task_text: [ tasks[cancel_index] ]
+            };
+        return $list.find('.todo-task').eq(cancel_index).replaceWith( HTML_template.render( new_task_template, data ) );
+    }
+    
+    // atnaujiname turini
+        // save click
+    function saveTask( event ) {
+        var save_index = $(event.target).parents('.todo-task').index(),
+            data = {
+                task_number: [ save_index + 1 ],
+                task_text: [  ]
+            };
+        tasks[save_index] = secureText( $list.find('.todo-task').eq(save_index).find('textarea').val() );
+        data.task_text.push( tasks[save_index] );
+
+        if ( tasks[save_index].length > 0 ) {
+            return $list.find('.todo-task').eq(save_index).replaceWith( HTML_template.render( new_task_template, data ) );
+        } else {
+            removeTask( event );
+        }
+
     }
     
     // atvaizduoti
@@ -67,7 +113,8 @@ var todo = (function(){
         tasks: tasks,
         add: addTask,
         update: updateTask,
-        remove: removeTask
+        remove: removeTask,
+        secure: secureText
     }
 
 })();
